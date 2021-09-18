c1d8c1800fd8a27e9f0baabdbaa435407277cd7f
aarontanjaya <aaront.tjy@gmail.com>
2021-09-18 20:17:44 +0700

    added mobilenet ssd and several test pics in trials

---
+++ CV/MobileNetSSDV2/MobileNetSSDv2test.py
@@ -0,0 +1,42 @@
+import cv2
+import time
+import numpy as np
+# Load a model imported from Tensorflow
+tensorflowNet = cv2.dnn.readNetFromTensorflow('ssd_mobilenet_v2_coco_2018_03_29.pb', 'ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+ 
+# Input image
+img = cv2.imread('../trials/kelasft1.jpg')
+rows, cols, channels = img.shape
+ 
+# Use the given image as input, which needs to be blob(s).
+tensorflowNet.setInput(cv2.dnn.blobFromImage(img, size=(320, 320), swapRB=True, crop=False))
+ 
+# Runs a forward pass to compute the net output
+t0 = time.time()
+networkOutput = tensorflowNet.forward()
+t = time.time()
+print("time",t-t0)
+print(networkOutput[0,0,0,0])
+print(type(networkOutput))
+print(networkOutput.shape)
+print(networkOutput[0].shape)
+print("tesshape")
+print(networkOutput[0].shape)
+# Loop on the outputs
+for detection in networkOutput[0,0]:
+    
+    score = float(detection[2])
+    if score > 0.2 and int(detection[1])==1:
+     
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+ 
+        #draw a red rectangle around detected objects
+        cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2)
+ 
+# Show the image with a rectagle surrounding the detected objects 
+cv2.imshow('Image', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
